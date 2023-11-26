de4c1b1d68c7d01971f0ede02d08ee49f9c9bdab
MD FAYSAL HOSSAIN <mdfaysalhossain019@gmail.com>
2023-11-26 12:34:10 +0600

    add
    26/11/2023

---
+++ sportStore/src/app/model/product.model.ts
@@ -0,0 +1,19 @@
+
+
+export class Product {
+
+    // id: number = 0;
+    // name: string = '';
+    // category: string = '';
+    // description: string = '';
+    // price: number = 0;
+    constructor(
+       public id ?: number,
+       public name ?: string,
+       public category ?: string,
+       public description ?: string,
+       public price ?: number,
+
+    ) { }
+
+}
